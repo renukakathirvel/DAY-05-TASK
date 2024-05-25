c0d700d644ec87f3f0e38e496697360c3bf11e4c
renukakathirvel <167777846+renukakathirvel@users.noreply.github.com>
2024-05-26 00:34:28 +0530

    adding the day05 task

---
+++ script.js
@@ -0,0 +1,45 @@
+var resume = {
+    "name":"renuka",
+    "age":22,
+    "mail id":"renukakathirvel18@gmail.com"
+    "professional summary":"Develop Web Applications The primary responsibility of full stack developer is to design and deliver full stack web applications.Looking for a IT job"
+    "skills":[
+             "Excellent communitioncation skills"
+             "Problem solving"
+             "Hard working person"
+             "Team work ability"
+             "quick learner"
+             ],
+    "work experience":"INFONET COMM ENTERPRISES PVT LTD (JULY2022/present)"           
+    "education":[
+      {
+                "Bachelor of science (computer science)"
+                "Hindustan college of arts and science (8.00-cgpa)"
+                
+      },
+      {
+                "Higher secondary education(53%)"
+                "Government girls higher secondary education(july2018/may2019)"
+      },
+      {
+                "Secondary education (90%)"
+                "Government girls higher secondary education(july2016/may2017)"
+      }
+],
+                
+
+    "languages":[
+                "Tamil"
+                "English"
+                ]
+              
+    
+    
+}
+for(var i =0;i<resume.length;i++){
+    console.log(arr[i]);
+  }
+for(var key in resume){
+   console.log(key,resume[key])
+}
+  
